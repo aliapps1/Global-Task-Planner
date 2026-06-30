f491a0d0bddb39d02c74c5a78b6114dff9e86f8e
aliapps1 <Abdulalish1970@gmail.com>
2026-06-30 14:35:06 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'premium_screen.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
